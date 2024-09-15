858ca855d5a7fdf2460d0b260290440b14fc082e
Isha Agarwal <isha.agarwal3139@gmail.com>
2024-09-14 22:19:06 -0400

    added routing logic to create home page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,13 +4,25 @@ import { randomName } from "@/Chat/randomName";
 import { Layout } from "@/Layout";
 import { UserMenu } from "@/components/UserMenu";
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "@/pages/Home";
 
 export default function App() {
   const [viewer] = useState(randomName());
   return (
-    <Layout menu={<UserMenu>{viewer}</UserMenu>}>
-      <ChatIntro />
-      <Chat viewer={viewer} />
-    </Layout>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>This is a test!</h1>} />
+          <Route path="/test" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
+  // return (
+  //   <Layout menu={<UserMenu>{viewer}</UserMenu>}>
+  //     <ChatIntro />
+  //     <Chat viewer={viewer} />
+  //   </Layout>
+  // );
 }
---
+++ my-app/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+// component for displaying an image during the game
+
+export function Home() {
+  return (
+    <>
+      <div>This is a test to see if the home page can work!</div>
+    </>
+  );
+}
