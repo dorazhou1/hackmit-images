73b878b6bf94b7c185b2668bd427373f1ce10897
Isha Agarwal <isha.agarwal3139@gmail.com>
2024-09-14 22:25:59 -0400

    added the chatapp to be the default page

---
+++ my-app/src/pages/ChatApp.tsx
@@ -0,0 +1,17 @@
+import { Chat } from "@/Chat/Chat";
+import { ChatIntro } from "@/Chat/ChatIntro";
+import { randomName } from "@/Chat/randomName";
+import { Layout } from "@/Layout";
+import { UserMenu } from "@/components/UserMenu";
+import { useState } from "react";
+
+export function ChatApp() {
+  const [viewer] = useState(randomName());
+
+  return (
+    <Layout menu={<UserMenu>{viewer}</UserMenu>}>
+      <ChatIntro />
+      <Chat viewer={viewer} />
+    </Layout>
+  );
+}
