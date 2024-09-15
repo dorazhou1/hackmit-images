d7b069006c933b4efbe6c79740630df0f4d7b22e
Dora Zhou <dorazhou1@gmail.com>
2024-09-14 23:04:55 -0400

    make selectimages

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,6 +7,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "@/pages/Home";
 import { ChatApp } from "@/pages/ChatApp";
+import { SelectImages } from "./pages/SelectImages";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<ChatApp />} />
           <Route path="/test" element={<Home />} />
+          <Route path="/selectimages" element={<SelectImages />} />
         </Routes>
       </Router>
     </>
---
+++ my-app/src/pages/SelectImages.tsx
@@ -0,0 +1,10 @@
+
+export function SelectImages() {
+  return (
+    <>
+      <div style={{ backgroundColor: "#a7e4fa", height: "100vh"}}>
+        
+      </div>
+    </>
+  );
+}
